2f4c596b970c4a208f590dbe16474422b815ddf8
eremaksoyy <fatmaeremaksoy@gmail.com>
2023-03-25 18:58:00 +0200

    Updated readme

---
+++ GUI_simulation/main.py
@@ -0,0 +1,86 @@
+import math
+import random
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_call():
+    return render_template('homePage.html')
+
+
+@app.route('/dragitems', methods=['GET'])
+def create_simulation():
+    return render_template('SimulationPage.html')
+
+
+@app.route('/help', methods=['GET'])
+def help_call():
+    return render_template('helpPage.html')
+
+
+@app.route('/contact', methods=['GET'])
+def contact_call():
+    return render_template('contact.html')
+
+
+@app.route('/input', methods=['GET', 'POST'])
+def get_inputs():
+    if request.method == 'POST':
+        # Get the values of numberOfEntities, lambda, and mu from the POST request
+        numberOfEntities = request.form['numberOfEntities']
+        lamda = request.form['lambda']
+        mu = request.form['mu']
+
+        # Convert the values to integers and floats
+        numberOfEntities = int(numberOfEntities)
+        lamda = float(lamda)
+        mu = float(mu)
+
+        # Calculate the MQL values
+        mql_values = calculate_mql(numberOfEntities, lamda, mu)
+        entities_list=[]
+        for i in range(numberOfEntities):
+            entities_list.append(i)
+
+        return render_template('simulation.html', mql_values=mql_values, entities_list=entities_list)
+    else:
+        return render_template('inputPage.html')
+
+
+def calculate_mql(Number_of_Entities, lamda, mue):
+    time_Arrival = 0
+    Iat = -1 / lamda * math.log(random.random())
+    Start_Serving = 0
+    entity_list = []
+    finish_Time = Start_Serving + mue
+    waiting = 0
+    total_waiting_time = 0
+    finish_list = []
+    MQL_analytic_list = []
+    mql_list = []
+    mql = 0
+    for i in range(int(Number_of_Entities)):
+        time_Arrival += Iat
+        # print("time_Arrival:", time_Arrival)
+        Iat = -(1 / lamda) * math.log(random.random())
+        # print("IAT : ", Iat)
+        Start_Serving = max(time_Arrival, finish_Time)
+        # print("Start_Serving: ", Start_Serving)
+        St = -(1 / mue) * math.log(random.random())
+        # print("St: ", St)
+        finish_Time = Start_Serving + St
+        entity_list.append(i)
+        finish_list.append(finish_Time)
+        waiting = finish_Time - time_Arrival
+        # print("Waiting: ", waiting)
+        mql += waiting
+        mql_list.append(mql / finish_Time)
+        total_waiting_time += waiting
+
+    return mql_list
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ GUI_simulation/static/javascript/plot.js
@@ -0,0 +1,38 @@
+function plotLineGraph(dataList, labelList) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: entities_list,
+            datasets: [{
+                label: 'Line Graph',
+                data: mql_values,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                tension: 0.3,   //set the tension property to a value between 0 and 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        padding: 20,
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Entities'
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Performance Measure Values'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+plotLineGraph(dataList, labelList);
