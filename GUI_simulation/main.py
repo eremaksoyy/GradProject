6d93c238a18c0f2fa81af042197d9042a9933e5e
eremaksoyy <fatmaeremaksoy@gmail.com>
2023-05-04 19:56:56 +0300

    "last version of the code without clickable values fixed"

--- GUI_simulation/main.py
+++ GUI_simulation/main.py
@@ -29,25 +29,21 @@ def contact_call():
 def get_inputs():
     if request.method == 'POST':
         # Get the values of numberOfEntities, lambda, and mu from the POST request
-        # array = request.form['lambda'].split(',')
-        # lamda = array[0]
-        # numberOfEntities = array[1]
-        lamda = request.form['lambda']
         numberOfEntities = request.form['numberOfEntities']
+        lamda = request.form['lambda']
         mu = request.form['mu']
+
         # Convert the values to integers and floats
         numberOfEntities = int(numberOfEntities)
         lamda = float(lamda)
         mu = float(mu)
-        queue_size = request.form['queueSize']
+
         # Calculate the MQL values
         mql_values = calculate_mql(numberOfEntities, lamda, mu)
         entities_list = []
         for i in range(numberOfEntities):
             entities_list.append(i)
-        outputs_list = calculateoutputs(lamda, mu, queue_size)
         return render_template('simulation.html', mql_values=mql_values, entities_list=entities_list)
-        #render_template('simulatemm1l.html', outputs_list=outputs_list)
 
     else:
         return render_template('inputPage.html')
@@ -84,31 +80,6 @@ def calculate_mql(Number_of_Entities, lamda, mue):
         total_waiting_time += waiting
 
     return mql_list
-
-
-def calculateoutputs(lamda, mue, queue_size):
-    Outputs_arr = []
-    Traffic_intensity = lamda / mue
-    Average_number_of_customers = Traffic_intensity / (1 - Traffic_intensity)
-    Average_system_time = Average_number_of_customers / lamda
-    Number_customers_queue = Average_number_of_customers * Traffic_intensity
-
-    if queue_size > Number_customers_queue:
-
-        Queue_time = Number_customers_queue / lamda
-
-    else:
-        Queue_time = queue_size / lamda
-
-    Outputs_arr.append(Traffic_intensity)
-    Outputs_arr.append(Average_number_of_customers)
-    Outputs_arr.append(Average_system_time)
-    Outputs_arr.append(Number_customers_queue)
-    Outputs_arr.append(Queue_time)
-
-    return Outputs_arr
-
-
 
 if __name__ == '__main__':
     app.run()
---
+++ GUI_simulation/static/javascript/barchart.js
@@ -0,0 +1,46 @@
+function fun()
+      {
+        var arriverate = document.getElementById('lambda').value;
+        var servicerate = document.getElementById('mu').value;
+        var L = document.getElementById('queuesize').value;
+        var traffic_intensity = arriverate/servicerate;
+        var average_num_of_customers = traffic_intensity/(1-traffic_intensity);
+        var average_system_time = average_num_of_customers/arriverate;
+        var num_of_customers_queue = average_num_of_customers * traffic_intensity;
+        if(L>num_of_customers_queue)
+        {
+          queue_time = num_of_customers_queue/arriverate;
+        }
+
+        else
+        {
+          queue_time = L/arriverate;
+        }
+        var xValues = ["Traffic Intensity", "Number Of Customers", "Average System Time", "Number Of Customers In The Queue", "Queue Time"];
+        var yValues = [traffic_intensity, average_num_of_customers, average_system_time, num_of_customers_queue, queue_time];
+        var barColors = ["red", "green","blue","orange","brown"];
+
+         barchart = new Chart("barChart", {
+            type: "bar",
+            data: {
+              labels: xValues,
+              datasets: [{
+                label:"Measured Value:",
+                backgroundColor: barColors,
+                  data: yValues
+                  }]
+            },
+         options: {
+          legend: {display: false},
+          title: {
+            display: false,
+            text: "M/M/1/L outputs"
+          }
+         }
+        });
+        barchart.update();
+        event.preventDefault();
+
+        }
+
+fun();
